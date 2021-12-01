115a3a00deb2bb40c7e1f341a90fd79aa0e26d6f
John Biesnecker <jbiesnecker@gmail.com>
2021-11-30 21:39:23 -0800

    2021 Day One

--- twentytwentyone/day_one.go
+++ twentytwentyone/day_one.go
@@ -1,11 +1,39 @@
 package twentytwentyone
 
-import "bufio"
+import (
+	"bufio"
+	"log"
+	"strconv"
+
+	"github.com/biesnecker/godvent/utils"
+)
+
+func readInputDayOne(fp *bufio.Reader) []int {
+	v, err := utils.ReadOneIntegerPerLine(fp)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return v
+}
 
 func DayOneA(fp *bufio.Reader) string {
-    return ""
+	nums := readInputDayOne(fp)
+	count := 0
+	for i := 0; i < len(nums)-1; i++ {
+		if nums[i] < nums[i+1] {
+			count++
+		}
+	}
+	return strconv.Itoa(count)
 }
 
 func DayOneB(fp *bufio.Reader) string {
-    return ""
+	nums := readInputDayOne(fp)
+	count := 0
+	for i := 0; i < len(nums)-3; i++ {
+		if nums[i]+nums[i+1]+nums[i+2] < nums[i+1]+nums[i+2]+nums[i+3] {
+			count++
+		}
+	}
+	return strconv.Itoa(count)
 }
