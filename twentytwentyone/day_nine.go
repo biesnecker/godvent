f892c89e7a76c0b7a68d2e9f8c7d6363272ebcd0
John Biesnecker <jbiesnecker@gmail.com>
2021-12-09 06:13:17 -0800

    2021 Day Nine

---
+++ twentytwentyone/day_nine.go
@@ -0,0 +1,86 @@
+package twentytwentyone
+
+import (
+	"bufio"
+	"math"
+	"sort"
+	"strconv"
+
+	"github.com/biesnecker/godvent/types"
+	"github.com/biesnecker/godvent/types/queue"
+	"github.com/biesnecker/godvent/utils"
+)
+
+func readInputDayNine(fp *bufio.Reader) map[types.Coord]int {
+	res := make(map[types.Coord]int)
+	utils.ReadStringsWithIndex(fp, func(i int, s string) {
+		for j := range s {
+			res[types.Coord{X: i, Y: j}] = int(s[j] - '0')
+		}
+	})
+	return res
+}
+
+func findLowPoints(m map[types.Coord]int) []types.Coord {
+	var res []types.Coord
+	for c, v := range m {
+		lowest := math.MaxInt
+		for _, adj := range c.GetAdjacentCoords() {
+			if depth, ok := m[adj]; ok {
+				if lowest > depth {
+					lowest = depth
+				}
+			}
+		}
+		if v < lowest {
+			res = append(res, c)
+		}
+	}
+	return res
+}
+
+func DayNineA(fp *bufio.Reader) string {
+	input := readInputDayNine(fp)
+	risk := 0
+	for _, loc := range findLowPoints(input) {
+		risk += input[loc] + 1
+	}
+	return strconv.Itoa(risk)
+}
+
+func DayNineB(fp *bufio.Reader) string {
+	input := readInputDayNine(fp)
+	claimed := make(map[types.Coord]bool)
+	var basins []int
+	for _, loc := range findLowPoints(input) {
+		q := queue.New()
+		q.Push(loc)
+		size := 0
+
+		for !q.Empty() {
+			c := q.Pop().(types.Coord)
+			for _, adj := range c.GetAdjacentCoords() {
+				if claimed[adj] {
+					continue
+				}
+				if v, ok := input[adj]; !ok {
+					continue
+				} else if v == 9 {
+					continue
+				}
+				claimed[adj] = true
+				q.Push(adj)
+				size++
+			}
+		}
+
+		basins = append(basins, size)
+	}
+
+	sort.Sort(sort.Reverse(sort.IntSlice(basins)))
+	prod := 1
+	for i := 0; i < 3; i++ {
+		prod *= basins[i]
+	}
+	return strconv.Itoa(prod)
+}
--- twentytwentyone/solutions.go
+++ twentytwentyone/solutions.go
@@ -20,5 +20,7 @@ func GetSolutions() map[string]types.Solution {
 		"year_2021_day_seven_b": {Input: "input/2021/day_seven.txt", Solution: DaySevenB},
 		"year_2021_day_eight_a": {Input: "input/2021/day_eight.txt", Solution: DayEightA},
 		"year_2021_day_eight_b": {Input: "input/2021/day_eight.txt", Solution: DayEightB},
+		"year_2021_day_nine_a":  {Input: "input/2021/day_nine.txt", Solution: DayNineA},
+		"year_2021_day_nine_b":  {Input: "input/2021/day_nine.txt", Solution: DayNineB},
 	}
 }
