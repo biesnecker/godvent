f22d958a1dc4172cec51815e2723692c91b0b05b
John Biesnecker <jbiesnecker@gmail.com>
2021-12-12 14:53:41 -0800

    2021 Day Twelve cleanup

--- twentytwentyone/day_twelve.go
+++ twentytwentyone/day_twelve.go
@@ -11,137 +11,85 @@ import (
 )
 
 type node struct {
-	label string
-	next  []string
-	big   bool
-}
-
-func copySeen(m map[string]bool) map[string]bool {
-	newm := make(map[string]bool)
-	for k, v := range m {
-		newm[k] = v
-	}
-	return newm
-}
-
-func copyPath(old []string) []string {
-	new := make([]string, len(old))
-	copy(new, old)
-	return new
-}
-
-func getNode(m map[string]*node, label string) *node {
-	if n, ok := m[label]; ok {
-		return n
-	} else {
-		big := unicode.IsUpper(rune(label[0]))
-		n := node{label: label, big: big}
-		m[label] = &n
-		return &n
-	}
+	label    string
+	next     []*node
+	smallIdx int
 }
 
 func readInputDayTwelve(fp *bufio.Reader) map[string]*node {
 	res := make(map[string]*node)
+	smallIdx := 1
+
+	getNode := func(label string) *node {
+		if n, ok := res[label]; ok {
+			return n
+		} else {
+			sidx := -1
+			if unicode.IsLower(rune(label[0])) {
+				if label == "start" {
+					sidx = 0
+				} else {
+					sidx = smallIdx
+					smallIdx++
+				}
+			}
+			n := node{label: label, smallIdx: sidx}
+			res[label] = &n
+			return &n
+		}
+	}
 
 	utils.ReadStrings(fp, func(s string) {
 		s = strings.ReplaceAll(s, "-", " ")
 		var left, right string
 		fmt.Sscanf(s, "%s %s", &left, &right)
-		leftN := getNode(res, left)
-		rightN := getNode(res, right)
+		leftN := getNode(left)
+		rightN := getNode(right)
 
-		leftN.next = append(leftN.next, right)
-		rightN.next = append(rightN.next, left)
+		leftN.next = append(leftN.next, rightN)
+		rightN.next = append(rightN.next, leftN)
 	})
 
 	return res
 }
 
-func getPath(
-	allNodes map[string]*node,
-	currentNode *node,
-	path []string,
-	seen map[string]bool,
-	allPaths map[string]bool) {
-
-	for _, next := range currentNode.next {
-		nn := allNodes[next]
-		if !nn.big && seen[nn.label] {
+func getPath(n *node, seen uint16, partTwo, doneTwice bool, count *int) {
+	for _, nn := range n.next {
+		if nn.label == "end" {
+			*count++
 			continue
 		}
 
-		if currentNode.label == "end" {
-			pj := strings.Join(path, "-")
-			allPaths[pj] = true
-			continue
-		}
-
-		newseen := copySeen(seen)
-		if !nn.big {
-			newseen[nn.label] = true
-		}
-		newpath := copyPath(path)
-		newpath = append(newpath, nn.label)
-
-		getPath(allNodes, nn, newpath, newseen, allPaths)
-
-	}
-
-}
-
-func DayTwelveA(fp *bufio.Reader) string {
-	input := readInputDayTwelve(fp)
-
-	allPaths := make(map[string]bool)
-
-	getPath(input, input["start"], []string{"start"}, map[string]bool{"start": true}, allPaths)
-	return strconv.Itoa(len(allPaths))
-}
-
-func getPath2(
-	allNodes map[string]*node,
-	currentNode *node,
-	path []string,
-	seen map[string]bool,
-	doneTwice bool,
-	allPaths map[string]bool) {
-
-	for _, next := range currentNode.next {
-		nn := allNodes[next]
 		newDoneTwice := doneTwice
-		if !nn.big && seen[nn.label] {
-			if doneTwice || nn.label == "start" {
+		if nn.smallIdx >= 0 && seen&(uint16(1)<<uint16(nn.smallIdx)) > 0 {
+			if !partTwo || nn.smallIdx == 0 {
+				continue
+			} else if partTwo && doneTwice {
 				continue
 			} else {
 				newDoneTwice = true
 			}
 		}
 
-		if currentNode.label == "end" {
-			pj := strings.Join(path, "-")
-			allPaths[pj] = true
-			continue
+		newseen := seen
+		if nn.smallIdx >= 0 {
+			newseen |= uint16(1) << uint16(nn.smallIdx)
 		}
-
-		newseen := copySeen(seen)
-		if !nn.big {
-			newseen[nn.label] = true
-		}
-		newpath := copyPath(path)
-		newpath = append(newpath, nn.label)
-
-		getPath2(allNodes, nn, newpath, newseen, newDoneTwice, allPaths)
-
+		getPath(nn, newseen, partTwo, newDoneTwice, count)
 	}
 
 }
 
+func DayTwelveA(fp *bufio.Reader) string {
+	input := readInputDayTwelve(fp)
+	count := 0
+	getPath(input["start"], 1, false, false, &count)
+	return strconv.Itoa(count)
+}
+
 func DayTwelveB(fp *bufio.Reader) string {
 	input := readInputDayTwelve(fp)
-
-	allPaths := make(map[string]bool)
-
-	getPath2(input, input["start"], []string{"start"}, map[string]bool{"start": true}, false, allPaths)
-	return strconv.Itoa(len(allPaths))
+	count := 0
+	getPath(input["start"], 1, true, false, &count)
+	return strconv.Itoa(count)
 }
